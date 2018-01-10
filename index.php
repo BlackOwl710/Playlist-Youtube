4ad1d1c1ccca0e01042ea68b831e91a3c5df4357
Unknown <yann.maillot.29@gmail.com>
2018-01-10 14:38:35 +0100

    Display video api Yt ok

--- index.php
+++ index.php
@@ -8,53 +8,91 @@
     <title>To watch</title>
 </head>
 <body>
-      <?php 
-        include (__DIR__.'/bdd/getco.php');
-        $bdd = get_co();
-        
-        $video_url = $bdd->prepare ('SELECT(url) FROM vids WHERE id=:id');
-        $video_url->execute(array(
-            'id'=>$_POST['retrivedId']
-        ));
-        
-        $video_url= $video_url->fetch();
-        $video_url= $video_url["url"]; 
+      <?php
+include __DIR__ . '/bdd/getco.php';
+$bdd = get_co();
 
-        echo ($video_url);
-        echo('<br>');
-        
-      
-        
-  ?>
-  </form>
-      <iframe width="560" height="315" src=<?php echo('"'.'https://www.youtube.com/embed/'.$video_url.'"');?> frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-        <?php echo('"'.'https://www.youtube.com/'.$video_url.'"');?>
-      <form id='inputform' action='forms/addVid.php' method='POST'>
-          <input name="insertVid" type='text' placeholder='Add your Videos here :)' />
-          <input type='submit'>
-      </form>
+$video_url = $bdd->prepare('SELECT(url) FROM vids WHERE id=:id');
+$video_url->execute(array(
+    'id' => $_POST['retrivedId'],
+));
+
+$video_url = $video_url->fetch();
+$video_url = $video_url["url"];
+
+echo ($video_url);
+echo ('<br>');
+
+?>
+
+      <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
+      <div id="player"></div>
+
+      <script>
+        // 2. This code loads the IFrame Player API code asynchronously.
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        // 3. This function creates an <iframe> (and YouTube player)
+        //    after the API code downloads.
+        var player;
+        function onYouTubeIframeAPIReady() {
+          player = new YT.Player('player', {
+            height: '360',
+            width: '640',
+            videoId: <?php echo("'".$video_url."'")?>,
+            events: {
+              'onReady': onPlayerReady,
+              'onStateChange': onPlayerStateChange
+            }
+          });
+        }
+
+        // 4. The API will call this function when the video player is ready.
+        function onPlayerReady(event) {
+          event.target.playVideo();
+        }
+
+        // 5. The API calls this function when the player's state changes.
+        //    The function indicates that when playing a video (state=1),
+        //    the player should play for six seconds and then stop.
+        var done = false;
+        function onPlayerStateChange(event) {
+          if (event.data == YT.PlayerState.PLAYING && !done) {
+            setTimeout(stopVideo, 6000);
+            done = true;
+          }
+        }
+        function stopVideo() {
+          player.stopVideo();
+        }
+      </script>
+
   <?php
 
-      $bdd = get_co();
+$bdd = get_co();
 
-        // $vids_id_user = $bdd->query('SELECT id FROM users');
-        // $vids= $bdd->query('SELECT * FROM vids WHERE user_id = $vids_id_user');
+// $vids_id_user = $bdd->query('SELECT id FROM users');
+// $vids= $bdd->query('SELECT * FROM vids WHERE user_id = $vids_id_user');
 
-        $vids = $bdd->prepare('SELECT * FROM vids');
-        $vids->execute();
-        $vids = $vids->fetchAll();
+$vids = $bdd->prepare('SELECT * FROM vids');
+$vids->execute();
+$vids = $vids->fetchAll();
 
-        if (!empty($vids)) {
-      foreach ($vids as $data) {
-  ?>
+if (!empty($vids)) {
+    foreach ($vids as $data) {
+        ?>
 
   <form class='addDelUpdt' action='index.php' method='POST'>
-    <input name='retrivedVid' type='text' value='<?php echo 'https://www.youtube.com/watch?v='.$data['url']; ?>'>
+    <input name='retrivedVid' type='text' value='<?php echo 'https://www.youtube.com/watch?v=' . $data['url']; ?>'>
     <input name='retrivedId' type='text' value='<?php echo $data['id']; ?>'>
     <input type="submit">
   </form>
-          
-  <?php }} ?>
+
+  <?php }}?>
 </body>
 
 
